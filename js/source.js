02ac3f6ace284b1758981be39bef41e77794e78e
amr kamal <amrfoad6@gmail.com>
2024-03-06 21:14:46 +0200

    added Math Equation

--- js/source.js
+++ js/source.js
@@ -60,3 +60,32 @@ lstbtn.forEach((el)=>{
  
 })
 //end of middle list buttons
+//using api 
+let apirates=fetch('https://api.currencyfreaks.com/v2.0/rates/latest?apikey=14ef191fa408457d89cfed2ddcc8f2df').then(
+    (res)=>{
+        let result=res.json();
+        return result; //return
+    }
+);
+
+//inputs configure
+let from=document.getElementsByClassName('from')[0];
+let to=document.getElementsByClassName('to')[0];
+let switchbtn=document.getElementsByClassName('change')[0];
+//Using ES6 to switch values of select elements
+switchbtn.addEventListener('click',()=>{
+    [from.value,to.value]=[to.value,from.value];
+})
+
+
+//Math operation Using Rates in Returned from resloving promise (API Currency rates)
+let amount=document.querySelector('.mid-container .inputs-container input');
+let convertbtn=document.querySelector('.mid-container .converter-div button');
+convertbtn.addEventListener('click',()=>{
+    apirates.then((res)=>{
+       beforeamount=amount.value/res.rates[from.value];
+       console.log(beforeamount);
+       afteramount=beforeamount*res.rates[to.value];
+       console.log(afteramount)
+    })
+});
