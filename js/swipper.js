a0904b63f562eea720d0d53d2a334c6e123c89b3
amr kamal <amrfoad6@gmail.com>
2024-03-19 04:29:54 +0200

    add slider recomend div

--- js/swipper.js
+++ js/swipper.js
@@ -1,4 +1,15 @@
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
 });
+let swiper2 = new Swiper(".mySwiper2", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    autoplay: {
+        delay: 3000, // Interval between slides in milliseconds (5 seconds in this example)
+        disableOnInteraction: false, // Prevents autoplay from being stopped when the user interacts with the swiper
+    },
+    speed: 1000,
+  });
